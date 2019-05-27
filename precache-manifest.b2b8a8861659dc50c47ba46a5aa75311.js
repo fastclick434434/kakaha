26ebc48f4b726e608fcec4e4fbd51130e9ef96f4
fastclick434434 <50154974+fastclick434434@users.noreply.github.com>
2019-05-27 20:22:00 +0300

    Add files via upload

---
+++ precache-manifest.b2b8a8861659dc50c47ba46a5aa75311.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13419e1f941eff3edf40d91baf4347a8",
+    "url": "/kakaha/index.html"
+  },
+  {
+    "revision": "5308d2a3e33e17c9d614",
+    "url": "/kakaha/static/css/2.b5769064.chunk.css"
+  },
+  {
+    "revision": "f2bf4bcd1a077250b12d",
+    "url": "/kakaha/static/css/main.957d3515.chunk.css"
+  },
+  {
+    "revision": "5308d2a3e33e17c9d614",
+    "url": "/kakaha/static/js/2.8ffd379c.chunk.js"
+  },
+  {
+    "revision": "f2bf4bcd1a077250b12d",
+    "url": "/kakaha/static/js/main.4eed4676.chunk.js"
+  },
+  {
+    "revision": "c8b7bd8105d49065071e",
+    "url": "/kakaha/static/js/runtime~main.1c918e8f.js"
+  },
+  {
+    "revision": "86a80ad23b7854cce5e409d71aeb8697",
+    "url": "/kakaha/static/media/icon.86a80ad2.jpg"
+  },
+  {
+    "revision": "54d23c2c772382c75fbf7d591a25413e",
+    "url": "/kakaha/static/media/image1.54d23c2c.png"
+  }
+]);
